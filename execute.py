4e9c450bd43118feea0319630048d0305724ff6a
abdulhafizfm13 <adetunjiadedeji86@gmail.com>
2021-08-09 07:25:30 -0700

    added an execute file

---
+++ execute.py
@@ -0,0 +1,5 @@
+git pull origin master
+In the last example, there is a new call to action on the page, right above the list of branches. It
+shows the name of the branch that you just created, and a big button for creating a PR. Click
+the button to continue; you should get to the Pull Request creation form, just like the Figure
+below.
